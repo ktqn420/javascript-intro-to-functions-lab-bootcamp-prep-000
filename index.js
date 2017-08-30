4f7abd4bc8d897f463db9b9de34104c570612a85
Anna Zhang <quinnkt9@gmail.com>
2017-08-30 22:16:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,18 +10,3 @@ function logShout(hello) {
 function logWhisper(HELLO) {
   console.log('hello')
 }
-function sayHiToGrandma(string){
-var uppercase = "HELLO"
-var lowercase = 'hello'
-var mixedCase = 'I love you, Grandma'
-
-lowercase.toLowerCase() === lowercase)
-console.log("I can't hear you!")
-
-uppercase.toUpperCase() === uppercase)
-console.log("YES. INDEED!")
-
-mixedCase.toLowerCase() === mixedCase
-mixedCase.toUpperCase() === mixedCase
-console.log("I love you, too.")
-}

766dfc93d7282c1224d0fed8d4331cf35d6742fa
Anna Zhang <quinnkt9@gmail.com>
2017-08-30 22:23:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ var uppercase = "HELLO"
 
 lowercase.toLowerCase() === lowercase)
 console.log("I can't hear you!")
-
+;
 var lowercase = 'hello'
 
 uppercase.toUpperCase() === uppercase)
 console.log("YES. INDEED!")
-
+;
 var mixedCase = 'I love you, Grandma'
 mixedCase.toLowerCase() === mixedCase
 mixedCase.toUpperCase() === mixedCase

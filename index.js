2f4f840389aec8255366348cdf6e0b081db38f5e
Anna Zhang <quinnkt9@gmail.com>
2017-08-30 19:16:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function sayHiToGrandma('hello') {
+function sayHiToGrandma(hello) {
   console.log('I can\'t hear you!')
   }

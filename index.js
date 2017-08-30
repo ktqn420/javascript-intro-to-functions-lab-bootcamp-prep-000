e6489a06f38dd60a17234e1b4e32d3bda963b963
Anna Zhang <quinnkt9@gmail.com>
2017-08-30 19:46:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,14 +12,13 @@ console.log('I can\'t hear you!' )
 }
 function sayHiToGrandma(string) {
   var uppercase = "HELLO!"
-   
+
   uppercase.toUpperCase() === uppercase
 console.log('YES INDEED!' )
 }
 function sayHiToGrandma (string) {
   var uppercase = "I LOVE YOU, GRANDMA."
-   
+
   uppercase.toUpperCase() === uppercase
   console.log("I love you, too.")
 }
-

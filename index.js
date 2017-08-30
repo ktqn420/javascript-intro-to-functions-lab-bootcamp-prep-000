0a3c2f638e8a4273c88d734b17181c967341ce1f
Anna Zhang <quinnkt9@gmail.com>
2017-08-30 22:00:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,17 +11,3 @@ function logWhisper('HELLO') {
   console.log('hello')
 }
 
-function sayHiToGrandma(string) {
-var uppercase = 'HELLO'
-if(uppercase.toUpperCase === uppercase){
-  console.log('YES INDEED!')
-}
-
-var lowercase = 'hello'
-if (lowercase.toLowerCase() === lowercase) {
-  console.log("I can't hear you!" )
-}
-
-function sayHiToGrandma('I love you, Grandma.' ) {
-  console.log('I love you, too.')
-}

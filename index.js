fe0f30615b6a7b7c56ff8a0e24c48aeb2e518bb4
Anna Zhang <quinnkt9@gmail.com>
2017-08-30 23:23:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ var lowercase = 'hello'
 
 function sayHiToGrandma(string) {
   if (lowercase.toLowerCase() === lowercase) {
-      return ("I love you, too.")
+      console.log("I can't hear you!" )
     }
